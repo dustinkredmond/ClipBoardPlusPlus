f401477a20b15654c0fcf0248b3a8de0d11d45a0
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2020-05-20 16:22:47 -0400

    Add graphic to dialog pane
    Signed-off-by: dredmond <dustinkredmond@gmail.com>

--- src/com/dustinredmond/controller/MainMenuController.java
+++ src/com/dustinredmond/controller/MainMenuController.java
@@ -2,11 +2,16 @@ package com.dustinredmond.controller;
 
 import com.dustinredmond.i18n.I18N;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class MainMenuController {
 
     public void showAboutInfo() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setGraphic(new ImageView("icons8-clipboard-64.png"));
+        ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("icons8-clipboard-64.png"));
         alert.setTitle(I18N.get("mi.about"));
         alert.setHeaderText(I18N.get("application.title"));
         alert.setContentText(I18N.get("mi.about.content"));
@@ -15,6 +20,8 @@ public class MainMenuController {
 
     public void showHelpInfo() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setGraphic(new ImageView("icons8-clipboard-64.png"));
+        ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("icons8-clipboard-64.png"));
         alert.setTitle(I18N.get("application.title"));
         alert.setHeaderText(I18N.get("application.title"));
         alert.setContentText(I18N.get("mi.help.content"));
